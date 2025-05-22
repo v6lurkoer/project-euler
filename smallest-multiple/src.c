eca0870abb10f2060dd30b41fa4ffd13cdd2f8da
Christopher <chris.kuut@pm.me>
2025-05-22 23:48:27 +0300

    It just worked lol

---
+++ smallest-multiple/src.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+const int MIN = 1;
+const int MAX = 20;
+
+int main(void)
+{
+    int num = 1;
+
+    while (1)
+    {
+        for (int i = MIN; i <= MAX; i++)
+        {
+            if ((num % i) != 0) break;
+            else
+            {
+                if (i == MAX)
+                {
+                    printf("%d\n", num);
+                    return 1;
+                }
+                continue;
+            }
+        }
+        num += 1;
+    }
+
+    return 0;
+}
